390c186c9660a0dd6ae08bf232286806379fd29c
Sven Neumann <sven@gimp.org>
2004-01-19 15:02:08 +0000

    no need to check for gtk-2.0.m4; we don't use it.
    2004-01-19  Sven Neumann  <sven@gimp.org>
    
    	* autogen.sh: no need to check for gtk-2.0.m4; we don't use it.

--- autogen.sh
+++ autogen.sh
@@ -119,7 +119,7 @@ fi
 if test -z "$ACLOCAL_FLAGS"; then
 
     acdir=`$ACLOCAL --print-ac-dir`
-    m4list="glib-gettext.m4 gtk-2.0.m4 intltool.m4 pkg.m4"
+    m4list="glib-gettext.m4 intltool.m4 pkg.m4"
 
     for file in $m4list
     do
