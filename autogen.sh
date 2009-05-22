572d569eddf43ad67bdf00053c53c56331f83a31
Wolfgang Hofer <wolfgangh@svn.gnome.org>
2009-05-22 21:44:24 +0200

    applied changes from gap-2-6 branch to master #583500 #583497 #583498

--- autogen.sh
+++ autogen.sh
@@ -88,7 +88,10 @@ fi
 
 
 echo -n "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
-if (automake-1.10 --version) < /dev/null > /dev/null 2>&1; then
+if (automake-1.11 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.11
+   ACLOCAL=aclocal-1.11
+elif (automake-1.10 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.10
    ACLOCAL=aclocal-1.10
 elif (automake-1.9 --version) < /dev/null > /dev/null 2>&1; then
